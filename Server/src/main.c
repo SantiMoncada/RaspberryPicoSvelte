e17cd43d01bd6236eb42e628121c8b70e01ce82e
Santiago Moncada <santiago.m.a@hotmail.com>
2024-03-10 23:04:44 +0100

    one file export

--- Server/src/main.c
+++ Server/src/main.c
@@ -21,6 +21,10 @@ int main()
 
     printf("Connected!\n");
 
+    extern cyw43_t cyw43_state;
+    auto ip_addr = cyw43_state.netif[CYW43_ITF_STA].ip_addr.addr;
+    printf("IP Address: %lu.%lu.%lu.%lu\n", ip_addr & 0xFF, (ip_addr >> 8) & 0xFF, (ip_addr >> 16) & 0xFF, ip_addr >> 24);
+
     httpd_init();
     printf("Http server initialised\n");
 
